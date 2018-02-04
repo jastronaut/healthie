05f5ac2bcd58ddcc0a6f98305e4467008591017c
Jas Delgado <41971x@gmail.com>
2018-02-04 09:26:13 -0800

    print errors for debugging purposes

--- foodrecs/views.py
+++ foodrecs/views.py
@@ -17,23 +17,23 @@ def enterinfo(request):
     p = Person()
     if request.method == "GET":
         form = CalculateCaloriesForm(request.GET)
-        # if form.is_valid():
-        p.age = request.GET.get("age")
-        print("age is", request.GET.get("age"))
-        p.weight = request.GET.get("weight")
-        p.height = request.GET.get("height")
-        p.activitylevel = request.GET.get("activitylevel")
-        p.gender = request.GET.get("gender")
-        # p.restrictions = form.cleaned_data["dietRestrictions"]
-        p.restrictions = request.GET.get("dietRestrictions")
-        p.update_info()
-        print("Ok, exiting to next page :) sweety")
-        return foodresults(request, p)
+        if form.is_valid():
+            p.age = request.GET.get("age")
+            print("age is", request.GET.get("age"))
+            p.weight = request.GET.get("weight")
+            p.height = request.GET.get("height")
+            p.activitylevel = request.GET.get("activitylevel")
+            p.gender = request.GET.get("gender")
+            # p.restrictions = form.cleaned_data["dietRestrictions"]
+            p.restrictions = request.GET.get("dietRestrictions")
+            p.update_info()
+            return foodresults(request, p)
             # return HttpResponseRedirect('')
             # return render(request, 'foodrecs/enterinfo.html', {'form': form, 'PageTitle': 'Enter Your Info', 'entered': True})
+        print("\terrors: ", form.errors)
     # print("Input not valid.")
-    # form = CalculateCaloriesForm()
-    # return render(request, 'foodrecs/enterinfo.html', {'form': form, 'PageTitle': 'Dang it!'})
+    form = CalculateCaloriesForm()
+    return render(request, 'foodrecs/enterinfo.html', {'form': form, 'PageTitle': 'Dang it!'})
 
 def foodresults(request, person):
     calories = get_calories(person.get_info("gender"), person.get_info("age"), person.get_info("height"), person.get_info("weight"), person.get_info("activitylevel"))
