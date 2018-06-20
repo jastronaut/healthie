afa744b78db38b33e3d1a2d61346ac64ea3a7d90
jastronaut <jas@jasdelgado.com>
2018-06-20 10:56:01 -0700

    remove old files, prepare to upload healthie as a django app

--- foodrecs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('foodresults', views.foodresults, name='foodresults'),
-    path('enterinfo', views.enterinfo, name="enterinfo"),
-]
